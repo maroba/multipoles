23f191e32260f9c76f84b23242e44e02c0933258
Matthias Baer <matthias.baer@mpl.mpg.de>
2022-09-15 17:30:43 +0200

    Prepare v0.3.0

--- multipoles/__init__.py
+++ multipoles/__init__.py
@@ -1,4 +1,4 @@
 from .expansion import MultipoleExpansion
 
-__version__ = '0.1.0'
+__version__ = '0.3.0'
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='multipoles',
-    version='0.2.1',
+    version='0.3.0',
     description='A Python package for multipole expansions of electrostatic or gravitational potentials',
     long_description="""*multipoles* is a Python package for multipole expansions of the solutions of the Poisson equation     
        (e.g. electrostatic or gravitational potentials). It can handle discrete and continuous charge or mass distributions.
@@ -11,18 +11,15 @@ setup(
     license='MIT',
     url='https://github.com/maroba/multipoles',
 
-    author='Matthias Baer',  # Optional
-    author_email='mrbaer@t-online.de',  # Optional
+    author='Matthias Baer',
+    author_email='matthias.r.baer@googlemail.com',
 
     classifiers=[
         'Intended Audience :: Developers',
         'Topic :: Scientific/Engineering :: Mathematics',
 
-        # Pick your license as you wish
         'License :: OSI Approved :: MIT License',
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
@@ -31,8 +28,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
-    keywords=['multipole expansion',  'physics', 'scientific-computing'],  # Optional
+    keywords=['multipole expansion',  'physics', 'scientific-computing'],
     packages=['multipoles'],
-    install_requires=['numpy', 'scipy'],  # Optional
+    install_requires=['numpy', 'scipy'],
 
 )
